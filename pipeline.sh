33db5999a9dfe0131eae1566e4611ab357dd6672
Salvador Medina <salvadom@andrew.cmu.edu>
2020-06-16 06:05:30 -0400

    Modularize pipeline script with diff paths

--- pipeline.sh
+++ pipeline.sh
@@ -1,13 +1,14 @@
 #!/usr/bin/env bash
 
 # Pipeline input variables
-NAME_LIST=/mnt/Alfheim/Data/OPERA/Venezuela/summer2020/FaceID/ner_per_filtered.lst
-FACEBANK_DIR=/mnt/Alfheim/Data/OPERA/Venezuela/summer2020/FaceID/facebank
-FACEBANK_LOG_PATH=/mnt/Alfheim/Data/OPERA/Venezuela/summer2020/FaceID/facebank/empty_dirs.lst
+FACEID_DIR=/mnt/Alfheim/Data/OPERA/Venezuela/summer2020/FaceID
+NAME_LIST=${FACEID_DIR}/ner_per_filtered.lst
+FACEBANK_DIR=${FACEID_DIR}/facebank
+FACEBANK_LOG_PATH=${FACEID_DIR}/empty_dirs.lst
 REID_RESULTS_PATH=./tmp/results.npy
-IMAGE_LIST=/mnt/Alfheim/Data/OPERA/Venezuela/summer2020/FaceID/image.lst
-IMAGE_DIR=/home/zal/Alfheim/Data/OPERA/Venezuela/summer2020/Venezuela_Data/text/20190926125226
-PIPELINE_OUTPUT_PATH=/mnt/Alfheim/Data/OPERA/Venezuela/summer2020/FaceID/output
+IMAGE_LIST=${FACEID_DIR}/image.lst
+DATA_DIR=/home/zal/Alfheim/Data/OPERA/Venezuela/summer2020/Venezuela_Data/text/20190926125226
+PIPELINE_OUTPUT_PATH=${FACEID_DIR}/output
 
 # Download the reference images
 python -u download_from_bing.py -i ${NAME_LIST} -n 5 -o ${FACEBANK_DIR}
@@ -16,10 +17,12 @@ python -u download_from_bing.py -i ${NAME_LIST} -n 5 -o ${FACEBANK_DIR}
 python -u validate_downloads.py -i ${FACEBANK_DIR} -l ${FACEBANK_LOG_PATH} --purge
 
 # First create the reference image database
+mkdir -p tmp
 python -u enroll.py -d ${FACEBANK_DIR}
 
 # Then label all the images in image list
 python -u recognize_list.py -i ${IMAGE_LIST} -o ${REID_RESULTS_PATH}
 
 # Finally export into the desired format
-python -u export_results.py -i ${REID_RESULTS_PATH} -o ${PIPELINE_OUTPUT_PATH}
+mkdir -p ${PIPELINE_OUTPUT_PATH}
+python -u export_results.py -i ${REID_RESULTS_PATH} -d ${DATA_DIR} -o ${PIPELINE_OUTPUT_PATH}
