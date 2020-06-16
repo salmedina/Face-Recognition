57874c7c15dc1bb589ede81fbaf8ec17688cb5c3
Salvador Medina <salvadom@andrew.cmu.edu>
2020-06-16 05:34:37 -0400

    Add bing download validation script

--- pipeline.sh
+++ pipeline.sh
@@ -3,6 +3,7 @@
 # Pipeline input variables
 NAME_LIST=/mnt/Alfheim/Data/OPERA/Venezuela/summer2020/FaceID/ner_per_filtered.lst
 FACEBANK_DIR=/mnt/Alfheim/Data/OPERA/Venezuela/summer2020/FaceID/facebank
+FACEBANK_LOG_PATH=/mnt/Alfheim/Data/OPERA/Venezuela/summer2020/FaceID/facebank/empty_dirs.lst
 REID_RESULTS_PATH=./tmp/results.npy
 IMAGE_LIST=/mnt/Alfheim/Data/OPERA/Venezuela/summer2020/FaceID/image.lst
 IMAGE_DIR=/home/zal/Alfheim/Data/OPERA/Venezuela/summer2020/Venezuela_Data/text/20190926125226
@@ -10,6 +11,9 @@ PIPELINE_OUTPUT_PATH=/mnt/Alfheim/Data/OPERA/Venezuela/summer2020/FaceID/output
 
 # Download the reference images
 python -u download_from_bing.py -i ${NAME_LIST} -n 5 -o ${FACEBANK_DIR}
+
+# Validate the downloaded images
+python -u validate_downloads.py -i ${FACEBANK_DIR} -l ${FACEBANK_LOG_PATH}
 
 # First create the reference image database
 python -u enroll.py -d ${FACEBANK_DIR}
---
+++ validate_downloads.py
@@ -0,0 +1,30 @@
+import argparse
+import os
+import shutil
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-i', '--input_dir', type=str, help='Directory to be verified if has empty sub-folders')
+    parser.add_argument('-l', '--log_path', type=str, help='Path to the file which will store the list of dirs which were empty')
+    parser.add_argument('--purge', action='store_true', help='Defines if the script must delete the empty directories')
+    return parser.parse_args()
+
+
+def main(opts):
+    empty_dirs_list = [os.path.basename(dirpath) for (dirpath, dirnames, filenames) in os.walk(opts.input_dir) if
+                       len(dirnames) == 0 and len(filenames) == 0]
+
+    if opts.purge:
+        for empty_dir_path in empty_dirs_list:
+            shutil.rmtree(empty_dir_path)
+
+    if len(empty_dirs_list) > 0:
+        with open(opts.log_path, 'w') as log_file:
+            log_file.write('\n'.join(empty_dirs_list))
+
+
+if __name__ == '__main__':
+    '''This script saves in a log file the list of empty directories. if purge is enabled, it deletes the empty dirs'''
+    opts = parse_args()
+    main(opts)
