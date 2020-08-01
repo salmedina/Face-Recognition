1484c3e0abe74280d16cb6d097e99bb238a93de7
Zaid Sheikh <zsheikh@cs.cmu.edu>
2020-08-01 14:30:30 -0400

    print exceptions and continue processing

--- recognize_list.py
+++ recognize_list.py
@@ -1,6 +1,8 @@
 import argparse
 import numpy as np
 import os.path as osp
+import sys
+import traceback
 
 def parse_args():
     parser = argparse.ArgumentParser()
@@ -48,25 +50,29 @@ if __name__ == "__main__":
     image_path_list = [l.strip() for l in open(args.input_list).readlines()]
     output_dict = dict()
     for idx, img_path in enumerate(tqdm(image_path_list)):
-        image = cv2.imread(img_path)
-        if image is None:
-            output_dict[img_path] = []
-            continue
-        image_original = image.copy()
-        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        try:
+            image = cv2.imread(img_path)
+            if image is None:
+                output_dict[img_path] = []
+                continue
+            image_original = image.copy()
+            image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 
-        faces = detect_faces(image)
+            faces = detect_faces(image)
 
-        face_list = []
-        for face in faces:
-            embedding = extract_face_embeddings(image, face, shape_predictor, face_recognizer)
-            label, score = recognize_face(embedding, embeddings, labels)
-            bbox = (face.left(), face.top(), face.right(), face.bottom())
-            if label != "Unknown":
-                face_list.append(dict(label=label,
-                                     score=score,
-                                     bbox=bbox,
-                                     embedding=embedding))
-        output_dict[osp.basename(img_path)] = face_list
+            face_list = []
+            for face in faces:
+                embedding = extract_face_embeddings(image, face, shape_predictor, face_recognizer)
+                label, score = recognize_face(embedding, embeddings, labels)
+                bbox = (face.left(), face.top(), face.right(), face.bottom())
+                if label != "Unknown":
+                    face_list.append(dict(label=label,
+                                         score=score,
+                                         bbox=bbox,
+                                         embedding=embedding))
+            output_dict[osp.basename(img_path)] = face_list
+        except Exception:
+            sys.stderr.write("ERROR: Exception occurred while processing {0}\n".format(img_path))
+            traceback.print_exc()
 
     np.save(args.output_path, output_dict)
